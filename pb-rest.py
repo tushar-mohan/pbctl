ed07bcd6bb5dbe964ee67736e3884921e8c1e29a
Tushar Mohan <tusharmohan@gmail.com>
2017-07-03 17:13:47 +0530

    added test to check REST service

--- pb-rest.py
+++ pb-rest.py
@@ -3,6 +3,7 @@ import sys, os
 import argparse
 from pbutils import configure_logging, jobs_list, browse_job, delete_job
 import logging
+import json
 from pprint import pprint
 
 logger = logging.getLogger('rest')
@@ -24,10 +25,10 @@ if __name__ == "__main__":
     configure_logging(verbose=args.verbose)
 
     if args.list:
-        pprint(jobs_list()['jobs'])
+        print json.dumps(jobs_list()['jobs'], indent=4)
     elif args.show:
         job_id = int(args.show)
-        pprint(browse_job(job_id)['perfdata'])
+        print json.dumps(browse_job(job_id)['perfdata'], indent=4)
     elif args.delete:
         job_id = int(args.delete)
         delete_job(job_id)
--- test/020_rest.bats
+++ test/020_rest.bats
@@ -1,12 +1,24 @@
 #!/usr/bin/env bats
 
 
-@test "data import followed by delete" {
+setup() {
     datafile="${PB_TEST_DIR}/data/sample.papiex.csv"
     [ -f "$datafile" ]
     run pb import "$datafile"
     [ $status -eq 0 ]
     job_id=$(echo ${output} | grep JobId | awk '{print $NF}')
+}
+
+@test "jobs listing" {
+    out=$(pb rest -l | grep '"id": ')
+    [[ "$out" =~ $job_id ]]
+}
+
+@test "job detail" {
+    out=$(pb rest -s $job_id | grep '"jobId": ')
+    [[ "$out" =~ $job_id ]]
+}
+
+teardown() {
     run pb rest -d $job_id
-    [ $status -eq 0 ]
 }
