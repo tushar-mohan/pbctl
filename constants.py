fa55da31a70d78be2ec8bbec448639907e2e4222
Tushar Mohan <tusharmohan@gmail.com>
2017-08-31 12:53:25 +0530

    renamed PB_BASE_URL -> PB_APIBASE_URL

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ from dotmap import DotMap
 import os
 
 K = DotMap()
-K.url.api.base = os.environ.get('PB_BASE_URL', 'http://127.0.0.1:5000/api/1.0')
+K.url.api.base = os.environ.get('PB_APIBASE_URL', 'http://127.0.0.1:5000/api/1.0')
 K.url.api.post.perfdata = K.url.api.base + '/perfdata/'
 K.url.api.userinfo = K.url.api.base + '/userinfo/'
 K.url.api.verify_auth = K.url.api.base + '/verify_auth/'
